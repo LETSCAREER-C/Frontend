5e069894d16301cbb129518e55728a63f53dced0
yuminnnnni <ka09023@ajou.ac.kr>
2024-09-03 20:45:56 +0900

    Feat: 추가  후기 컴포넌트 & 페이지 생성, 목록 페이지 컴포넌트 디자인, 기능 구현, 목록 페이지, 후기 페이지 라우팅 설정
    Related to: #2

--- src/assets/svg/index.ts
+++
@@ -1,8 +0,0 @@
-export { default as Blog } from './Blog';
-export { default as Career } from './Career';
-export { default as Hamburger } from './Hamburger';
-export { default as Insta } from './Insta';
-export { default as Lets } from './Lets';
-export { default as LetsDesk } from './LetsDesk';
-export { default as Logined } from './Logined';
-export { default as Xclose } from './Xclose';
