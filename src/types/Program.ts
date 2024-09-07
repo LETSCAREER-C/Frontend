33c96c8c6c9c70f857a1bc7adb111c63c038d189
yuminnnnni <ka09023@ajou.ac.kr>
2024-09-07 21:55:57 +0900

    Feat: axios 코드 추가

--- src/types/Program.ts
+++ src/types/Program.ts
@@ -1,14 +1,14 @@
-import { RecruitState } from "../components/Filters/RecruitTags";
+import { RecruitState } from '../components/Filters/RecruitTags';
 
 export interface Program {
-  id: number;
+  programId: number;
   title: string;
   intro: string;
-  dday: number;
-  program_start_date: string;
-  program_finish_date: string;
-  imageUrl: string;
-  status: RecruitState;
-  career_type: string;
-  program_type: string;
+  deadline: number;
+  programStartDate: string;
+  programEndDate: string;
+  thumbnail: string;
+  recruitStatus: RecruitState;
+  tag: string;
+  dtype: string;
 }
